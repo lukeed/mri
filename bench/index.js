04d13f4d6cb372376cdf2eef119eb11da0682004
Luke Edwards <luke.edwards05@gmail.com>
2018-12-21 01:06:24 -0800

    chore: update benchmark

--- bench/index.js
+++ bench/index.js
@@ -1,37 +1,18 @@
-const Table = require('cli-table2');
-const { Suite } = require('benchmark');
-const yargs = require('yargs-parser');
-const minimist = require('minimist');
+const nopt = require('nopt');
 const previous = require('mri');
-const curr = require('../lib');
+const yargs = require('yargs-parser');
+const { Suite } = require('benchmark');
+const minimist = require('minimist');
+const mri = require('../lib');
 
 const bench = new Suite();
 const args = ['-b', '--bool', '--no-meep', '--multi=baz'];
 
 bench
-	.add('mri', () => curr(args))
-	.add('yargs', () => yargs(args))
-	.add('minimist', () => minimist(args))
-	.add('mri (previous)', () => previous(args))
+	.add('minimist    ', () => minimist(args))
+	.add('mri (prev)  ', () => previous(args))
+	.add('mri         ', () => mri(args))
+	.add('nopt        ', () => nopt(args))
+	.add('yargs-parser', () => yargs(args))
 	.on('cycle', e => console.log(String(e.target)))
-	.on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
-
-		const tbl = new Table({
-			head: ['Name', 'Mean time', 'Ops/sec', 'Diff']
-		});
-
-		let prev, diff;
-
-		bench.forEach(el => {
-			if (prev) {
-				diff = ((el.hz - prev) * 100 / prev).toFixed(2) + '% faster';
-			} else {
-				diff = 'N/A'
-			}
-			prev = el.hz;
-			tbl.push([el.name, el.stats.mean, el.hz.toLocaleString(), diff])
-		});
-		console.log(tbl.toString());
-	})
 	.run();
