b2459b6c27ce9a1d70fcf8a1e5eb4d613062bc90
FrankFang <frankfang1990@gmail.com>
2023-06-09 04:35:42 +0800

    Make the code more readable

--- src/index.js
+++ src/index.js
@@ -1,116 +1,164 @@
-function toArr(any) {
+function toArray(any) {
 	return any == null ? [] : Array.isArray(any) ? any : [any];
 }
 
-function toVal(out, key, val, opts) {
-	var x, old=out[key], nxt=(
-		!!~opts.string.indexOf(key) ? (val == null || val === true ? '' : String(val))
-		: typeof val === 'boolean' ? val
-		: !!~opts.boolean.indexOf(key) ? (val === 'false' ? false : val === 'true' || (out._.push((x = +val,x * 0 === 0) ? x : val),!!val))
-		: (x = +val,x * 0 === 0) ? x : val
-	);
-	out[key] = old == null ? nxt : (Array.isArray(old) ? old.concat(nxt) : [old, nxt]);
+function getNumberOrString(str) {
+	const n = +str
+	return (+n) * 0 === 0 ? n : str
+}
+function setValue(out, key, value, options) {
+	let prev = out[key]
+	let next
+	if (options.string.indexOf(key) !== -1) {
+		next = value == null || value === true ? '' : String(value);
+		if (value === null || value === undefined || value === true) {
+			next = '';
+		} else {
+			next = String(value);
+		}
+	} else if (typeof value === 'boolean') {
+		next = value;
+	} else if (options.boolean.indexOf(key) !== -1) {
+		if (value === 'false') {
+			next = false
+		} else if (value === 'true') {
+			next = true
+		} else {
+			out._.push(getNumberOrString(value))
+			next = !!value
+		}
+	} else {
+		next = getNumberOrString(value)
+	}
+
+	if (prev === undefined || prev === null) {
+		out[key] = next
+	} else {
+		out[key] = Array.isArray(prev) ? prev.concat(next) : [prev, next]
+	}
 }
 
-export default function (args, opts) {
+export default function (args, options) {
 	args = args || [];
-	opts = opts || {};
+	options = options || {};
 
-	var k, arr, arg, name, val, out={ _:[] };
-	var i=0, j=0, idx=0, len=args.length;
+	const out = { _: [] };
+	const len = args.length;
 
-	const alibi = opts.alias !== void 0;
-	const strict = opts.unknown !== void 0;
-	const defaults = opts.default !== void 0;
+	const hasAliases = options.alias !== void 0;
+	const strict = options.unknown !== void 0;
+	const hasDefaults = options.default !== void 0;
 
-	opts.alias = opts.alias || {};
-	opts.string = toArr(opts.string);
-	opts.boolean = toArr(opts.boolean);
+	options.alias = options.alias || {};
+	options.string = toArray(options.string);
+	options.boolean = toArray(options.boolean);
 
-	if (alibi) {
-		for (k in opts.alias) {
-			arr = opts.alias[k] = toArr(opts.alias[k]);
-			for (i=0; i < arr.length; i++) {
-				(opts.alias[arr[i]] = arr.concat(k)).splice(i, 1);
+	if (hasAliases) {
+		// convert alias
+		// from { h: ['he', 'help'] }
+		// to   { h: ['he', 'help'], he: ['h', 'help'], help: ['h', 'he'] }
+		for (let k in options.alias) {
+			const aliasValues = options.alias[k] = toArray(options.alias[k]);
+			for (let i = 0; i < aliasValues.length; i++) {
+				const k2 = aliasValues[i]
+				options.alias[k2] = aliasValues.concat(k)
+				options.alias[k2].splice(i, 1)
 			}
 		}
 	}
 
-	for (i=opts.boolean.length; i-- > 0;) {
-		arr = opts.alias[opts.boolean[i]] || [];
-		for (j=arr.length; j-- > 0;) opts.boolean.push(arr[j]);
+	// convert { alias: {b: 'bar'}, boolean: 'b' }
+	// to { alias: {b: ['bar']}, boolean: ['b', 'bar'] }
+	for (let i = options.boolean.length - 1; i >= 0; i--) {
+		const arg = options.boolean[i]
+		const aliases = options.alias[arg] || [];
+		options.boolean.push.apply(options.boolean, aliases);
 	}
 
-	for (i=opts.string.length; i-- > 0;) {
-		arr = opts.alias[opts.string[i]] || [];
-		for (j=arr.length; j-- > 0;) opts.string.push(arr[j]);
+
+	// convert { alias: {b: 'bar'}, string: 'b' }
+	// to { alias: {b: ['bar']}, string: ['b', 'bar'] }
+	for (let i = options.string.length - 1; i >= 0; i--) {
+		const arg = options.string[i]
+		const aliases = options.alias[arg] || [];
+		options.string.push.apply(options.string, aliases);
 	}
 
-	if (defaults) {
-		for (k in opts.default) {
-			name = typeof opts.default[k];
-			arr = opts.alias[k] = opts.alias[k] || [];
-			if (opts[name] !== void 0) {
-				opts[name].push(k);
-				for (i=0; i < arr.length; i++) {
-					opts[name].push(arr[i]);
-				}
+	if (hasDefaults) {
+		// convert { default: { foo:true }, alias: { f:'foo' } }
+		// to  { default: { foo:true }, alias: { f: 'foo' }, boolean: ['foo', 'f'] }
+		for (let k in options.default) {
+			const type = typeof options.default[k];
+			const aliases = options.alias[k] = options.alias[k] || [];
+			if (options[type] !== undefined) {
+				options[type].push(k);
+				options[type].push.apply(options[type], aliases);
 			}
 		}
 	}
 
-	const keys = strict ? Object.keys(opts.alias) : [];
+	const keys = strict ? Object.keys(options.alias) : [];
 
-	for (i=0; i < len; i++) {
-		arg = args[i];
+	for (let i = 0; i < len; i++) {
+		let arg = args[i];
 
 		if (arg === '--') {
-			out._ = out._.concat(args.slice(++i));
+			out._ = out._.concat(args.slice(i + 1));
 			break;
 		}
 
-		for (j=0; j < arg.length; j++) {
-			if (arg.charCodeAt(j) !== 45) break; // "-"
+		let j
+		for (j = 0; j < arg.length; j++) {
+			if (arg.charCodeAt(j) !== 45) break; // consume all the "-" characters
 		}
 
 		if (j === 0) {
 			out._.push(arg);
 		} else if (arg.substring(j, j + 3) === 'no-') {
-			name = arg.substring(j + 3);
-			if (strict && !~keys.indexOf(name)) {
-				return opts.unknown(arg);
+			const name = arg.substring(j + 3);
+			if (strict && keys.indexOf(name) === -1) {
+				return options.unknown(arg);
 			}
 			out[name] = false;
 		} else {
-			for (idx=j+1; idx < arg.length; idx++) {
-				if (arg.charCodeAt(idx) === 61) break; // "="
+			let idx
+			for (idx = j + 1; idx < arg.length; idx++) {
+				if (arg.charCodeAt(idx) === 61) break; // find a "="
 			}
 
-			name = arg.substring(j, idx);
-			val = arg.substring(++idx) || (i+1 === len || (''+args[i+1]).charCodeAt(0) === 45 || args[++i]);
-			arr = (j === 2 ? [name] : name);
+			let name = arg.substring(j, idx);
+			let value = arg.substring(idx + 1)
+			if (value === '') {
+				if (i === len - 1 || args[i + 1].toString().charCodeAt(0) === 45) {
+					value = true
+				} else {
+					value = args[i + 1]
+					i += 1
+				}
+			}
+			const names = (j === 2 ? [name] : name);
 
-			for (idx=0; idx < arr.length; idx++) {
-				name = arr[idx];
-				if (strict && !~keys.indexOf(name)) return opts.unknown('-'.repeat(j) + name);
-				toVal(out, name, (idx + 1 < arr.length) || val, opts);
+			for (let idx = 0; idx < names.length; idx++) {
+				const name = names[idx];
+				if (strict && keys.indexOf(name) === -1) return options.unknown('-'.repeat(j) + name);
+				setValue(out, name, idx < names.length - 1 ? true : value, options);
 			}
 		}
 	}
 
-	if (defaults) {
-		for (k in opts.default) {
-			if (out[k] === void 0) {
-				out[k] = opts.default[k];
+	if (hasDefaults) {
+		for (let k in options.default) {
+			if (out[k] === undefined) {
+				out[k] = options.default[k];
 			}
 		}
 	}
 
-	if (alibi) {
-		for (k in out) {
-			arr = opts.alias[k] || [];
-			while (arr.length > 0) {
-				out[arr.shift()] = out[k];
+	if (hasAliases) {
+		for (let k in out) {
+			const aliases = options.alias[k] || [];
+			while (aliases.length > 0) {
+				out[aliases.shift()] = out[k];
 			}
 		}
 	}
